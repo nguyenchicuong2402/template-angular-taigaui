f1e007763452dd90332e9137eb0c277df53d6dba
Nguyễn Chí Cường <nguyenchicuong2402@gmail.com>
2022-07-23 22:19:31 +0700

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, TuiRootModule, BrowserAnimationsModule, TuiDialogModule, TuiAlertModule],
+    imports: [BrowserModule, AppRoutingModule, TuiRootModule, BrowserAnimationsModule, TuiDialogModule, TuiAlertModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
     bootstrap: [AppComponent],
 })
